5e41800e669f96dac382383d0459121c664e9b5f
cab2024 <170064810+cab2024@users.noreply.github.com>
2025-03-09 18:32:38 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -30,7 +30,7 @@ PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-# 服务——FileBrowser 用户名admin 密码admin
+# 服务——FileBrowser 用户名root 密码
 PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
